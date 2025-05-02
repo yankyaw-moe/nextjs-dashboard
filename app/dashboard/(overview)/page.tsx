94a028d8851cc907c38d20eea40bd6cbbfa74590
Yan Kyaw Moe <yankyawmoe.dev@gmail.com>
2025-05-02 13:33:33 +0630

    ch14: learned how to improve accessibility in your forms with React Form Status and Server-side Validation.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,11 +3,11 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import {
-  // fetchRevenue,
-  // fetchLatestInvoices,
-  fetchCardData,
-} from "@/app/lib/data";
+// import {
+//   // fetchRevenue,
+//   // fetchLatestInvoices,
+//   fetchCardData,
+// } from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
@@ -18,12 +18,12 @@ import {
 export default async function Page() {
   // const revenue = await fetchRevenue();
   // const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+  // const {
+  //   numberOfCustomers,
+  //   numberOfInvoices,
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  // } = await fetchCardData();
   // const [
   //   revenue,
   //   latestInvoices,
